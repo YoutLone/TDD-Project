399121033f8bf22fa208d7fadc81eba71561280f
Yout <youtlone95@gmail.com>
2023-09-08 13:56:21 +0630

    fix linter error

--- solver.rb
+++ solver.rb
@@ -1,27 +1,23 @@
 class Solver
-    def factorial(number)
-        raise ArgumentError, 'Factorial is not defined for negative integers.' if number.negative?
-    
-        result = (1..number).reduce(1, :*)
-        result
-    end
-  
-    def reverse(word)
-        result = word.reverse
-        result
-    end
-  
-    def fizzbuzz(number)
-        result = if (number % 3).zero? && (number % 5).zero?
-                   'fizzbuzz'
-                 elsif (number % 3).zero?
-                   'fizz'
-                 elsif (number % 5).zero?
-                   'buzz'
-                 else
-                   number.to_s
-                 end
-        result
-      end
+  def factorial(number)
+    raise ArgumentError, 'Factorial is not defined for negative integers.' if number.negative?
+
+    (1..number).reduce(1, :*)
   end
-  
+
+  def reverse(word)
+    word.reverse
+  end
+
+  def fizzbuzz(number)
+    if (number % 3).zero? && (number % 5).zero?
+      'fizzbuzz'
+    elsif (number % 3).zero?
+      'fizz'
+    elsif (number % 5).zero?
+      'buzz'
+    else
+      number.to_s
+    end
+  end
+end
